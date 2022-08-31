c03164d986d9a94947ff2b7029d9e34a7bbff14a
leewooyup <zjadlspun1114@naver.com>
2022-08-31 15:50:44 +0900

    TestInitData 도입

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryImpl.java
@@ -1,5 +1,4 @@
 package com.ll.exam.qsl.user.repository;
-
 import com.ll.exam.qsl.user.entity.SiteUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +8,15 @@ import static com.ll.exam.qsl.user.entity.QSiteUser.siteUser;
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public SiteUser getQslUser(Long id) {
+        /*
+        SELECT *
+        FROM site_user
+        WHERE id = 1
+        */
+
         return jpaQueryFactory
                 .select(siteUser)
                 .from(siteUser)
